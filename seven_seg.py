ad69912a58cd79e5e9070082b95821f631a884ce
Matthew Wang <38610107+matowang@users.noreply.github.com>
2019-12-14 16:19:25 +0800

    fix bugs

--- seven_seg.py
+++ seven_seg.py
@@ -1,47 +1,57 @@
 from machine import PIN
-from enum import Enum
-class Common(enum):
+class Common:
   ANODE = 0
   CATHODE = 1
 
 class SevenSeg:
-
-  str_ecoding = {
-    '0': (1, 1, 1, 1, 1, 1, 0),
-    '1': (0, 1, 1, 0, 0, 0, 0)
-  }
-
-  def __init__(self, A, B, C, D, E, F, G, DP, common, power):  
-    self.common = common
-    self.off_value = 0 if common == Common.CATHODE else 1
-    self.on_value = 1 if common == Common.CATHODE else 0
-    init_value = self.off_value
+    str_encoding = {
+        '0': (1, 1, 1, 1, 1, 1, 0),
+        '1': (0, 1, 1, 0, 0, 0, 0),
+        '2': (1, 1, 0, 1, 1, 0, 1),
+        '3': (1, 1, 1, 1, 0, 0, 1),
+        '4': (0, 1, 1, 0, 0, 1, 1),
+        '5': (1, 0, 1, 1, 0, 1, 1),
+        '6': (1, 0, 1, 1, 1, 1, 1),
+        '7': (1, 1, 1, 0, 0, 0, 0),
+        '8': (1, 1, 1, 1, 1, 1, 1),
+        '9': (1, 1, 1, 1, 0, 1, 1),
+        'A': (1, 1, 1, 0, 1, 1, 1),
+    }
+    def __init__(self, A, B, C, D, E, F, G, DP, common):
+        self.common = common
+        if common == Common.CATHODE:
+            self.off_value = 0
+            self.on_value = 1
+        else:
+            self.off_value = 1
+            self.on_value = 0
     
-    self.PINS = {A,B,C,D,E,F,G}
-    
-    for pin in self.PINS:
-      pin = PIN(
-        id='P' + pin, 
-        mode=PIN.OUT, 
-        value=init_value, 
-        drive=power)
-        
-  def on_all(self):
-    for pin in self.PINS.values():
-      pin.value(self.on_value)
-      
-  def off_all(self):
-    for pin in self.PINS.values():
-      pin.value(self.off_value)
-      
-  def light_segs(self, A,B,C,D,E,F,G):
-    input = {A,B,C,D,E,F,G}
-    for i in range(7):
-      if input[i]:
-        self.PINS[i].value(on_value)
-      else:
-        self.PINS[i].value(off_value)
-  
-  def draw(s):
-    code = SevenSeg.str_encoding[s]
-    self.light_segs(code)
+        init_value = self.off_value
+
+        self.PINS = [A,B,C,D,E,F,G]
+
+        for i in range(7):
+            print(i)
+            self.PINS[i] = PIN('P{}'.format(self.PINS[i]))
+            self.PINS[i].init(mode=PIN.OUT, value=init_value)
+
+    def on_all(self):
+        for pin in self.PINS:
+            pin.value(self.on_value)
+
+    def off_all(self):
+        for pin in self.PINS:
+            pin.value(self.off_value)
+
+    def light_segs(self, A,B,C,D,E,F,G):
+        input = [A,B,C,D,E,F,G]
+        for i in range(7):
+            if input[i]:
+                self.PINS[i].value(self.on_value)
+            else:
+                self.PINS[i].value(self.off_value)
+
+    def draw(self, s):
+        code = SevenSeg.str_encoding[str(s)]
+        print(*code)
+        self.light_segs(*code)
